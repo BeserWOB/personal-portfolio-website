ce043bfeaf2a74fb955c96d4c2bf08b4002db276
BeserWOB <beser574@gmail.com>
2023-05-08 23:33:06 +0200

    Projects section almost finished.

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, {useEffect} from "react";
+import projectsData from "../projectsData";
+
 
 
 export default function Projects (){
+
+    useEffect(()=>{
+        const panels = document.querySelectorAll(".panel");
+       
+        panels[0].classList.add('active');
+        
+        panels.forEach((panel) =>{
+            panel.addEventListener('click', () =>{
+                removeActiveClass();
+                panel.classList.add('active')
+            })
+        })
+        const removeActiveClass = () => {
+            panels.forEach(panel => {
+                panel.classList.remove('active');
+            })
+        }   
+    },[])    
+
+
+
+
     return (
-        <div className="hero-section">
-            <h1>Projects</h1>
+        <>
+            <div className="projects-section">
+            <h1 className="projects-title">Projects</h1>
+                <div className="projects-container">
+                {projectsData.map((project, i) => 
+                    <div 
+                        className="panel"
+                        key={i}    
+                        >
+                        <img 
+                            src={project.url} 
+                            alt="project screenshot" 
+                        />
+                        <h3>{project.title}</h3>
+                        <p className="github">{project.github}</p>
+                        <p className="netlify">{project.netlify}</p>
+                    </div>
+                )}
+            </div>
         </div>
+        </>
     )
 }
---
+++ projectsData.js
@@ -0,0 +1,39 @@
+export default [
+    {    
+        url: "./images/nature1.jpeg",
+        title: "Project Title1",
+        github: "github url",
+        netlify: "netlify url"
+    },
+    {    
+        url: "./images/nature2.jpeg",
+        title: "Project Title2",
+        github: "github url",
+        netlify: "netlify url"
+    },
+    {    
+        url: "./images/nature3.jpeg",
+        title: "Project Title3",
+        github: "github url",
+        netlify: "netlify url"
+    },
+    {    
+        url: "./images/nature4.jpeg",
+        title: "Project Title4",
+        github: "github url",
+        netlify: "netlify url"
+    },
+    {    
+        url: "./images/nature5.jpeg",
+        title: "Project Title5",
+        github: "github url",
+        netlify: "netlify url"
+    },
+    {    
+        url: "./images/nature6.jpeg",
+        title: "Project Title6",
+        github: "github url",
+        netlify: "netlify url"
+    },
+
+]
