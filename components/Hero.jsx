7f67f8d7846eef5cb47baaac5d1bca8e782fd832
BeserWOB <beser574@gmail.com>
2023-05-09 17:21:44 +0200

    sasa

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,8 +5,8 @@ export default function Hero (){
     return (
         <div className="hero-section">
             <h1>Ivan Beserminji's Portfolio of Projects</h1>
-            <p>Welcome to my portfolio! This is the place where you can explore my latest projects and get a sense of my design style and creative approach. From web design to branding, I've had the pleasure of working on a variety of exciting projects that I'm excited to share with you. Take a look around and get inspired by my work.</p>
             <img src="./images/ivan-profile-pic.jpeg" alt="Ivan profile picture" />
+            <p>Welcome to my portfolio! This is the place where you can explore my latest projects and get a sense of my design style and creative approach. From web design to branding, I've had the pleasure of working on a variety of exciting projects that I'm excited to share with you. Take a look around and get inspired by my work.</p>
         </div>
     )
 }
