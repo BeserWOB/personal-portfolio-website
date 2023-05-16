b519ae1c384992df790ad327613c51e963f924cc
BeserWOB <beser574@gmail.com>
2023-05-16 10:40:04 +0200

    Image problem debbuging, first try.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,11 +5,11 @@ export default function Footer (){
     return (
         <footer>
             <div className="footer-icons">
-                <img src="../images/footer/facebook.png" alt="facebook-logo" />
-                <img src="../images/footer/instagram.png" alt="instagram-logo" />
-                <img src="../images/footer/github.png" alt="github-logo" />
-                <img src="../images/footer/whatsapp.png" alt="whatsapp-logo" />
-                <img src="../images/footer/youtube.png" alt="youtube-logo" />
+                <img src="./images/footer/facebook.png" alt="facebook-logo" />
+                <img src="./images/footer/instagram.png" alt="instagram-logo" />
+                <img src="./images/footer/github.png" alt="github-logo" />
+                <img src="./images/footer/whatsapp.png" alt="whatsapp-logo" />
+                <img src="./images/footer/youtube.png" alt="youtube-logo" />
             </div>
                 <p>Made by Ivan Beserminji with React in Wolfsburg 2023.</p>
         </footer>
